0b21df012435aa81cacf8d7f0013c9b8839dc3d2
cermak <cermak@cermak.dszo>
2021-12-03 10:59:41 +0100

    update

---
+++ PrevodStupnu/src/prevodstupnu/Prevodnik.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prevodstupnu;
+
+/**
+ *
+ * @author cermak
+ */
+public class Prevodnik {
+    
+    
+    public static double PrevodNaStupne(double radiany){
+        double stupne = radiany * 180 / Math.PI;
+    
+    return stupne;
+    
+    }
+      public static double PrevodNaRadiany(double stupne){
+        double radiany = //doplni t vypocet;
+    
+    return radiany;
+    
+    }
+    
+}
