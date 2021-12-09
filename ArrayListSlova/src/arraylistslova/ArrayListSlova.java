5edbbb0186245a27af9f02441596df7983cd0da9
cermak <cermak@cermak.dszo>
2021-12-09 14:31:14 +0100

    slova

---
+++ ArrayListSlova/src/arraylistslova/ArrayListSlova.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraylistslova;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author cermak
+ */
+public class ArrayListSlova {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String slovo= "";
+        ArrayList<String> seznam = new ArrayList<String>();
+        //vytvoreni objektu scanneru
+        Scanner vstup = new Scanner(System.in);
+            
+            
+        while (!slovo.equals("konec")  ) { 
+            
+              slovo = vstup.nextLine();   
+             
+              
+        }
+                
+            
+        
+        
+     
+        
+        
+        
+        }
+        
+      
+        
+        
+    }
+    
+  
