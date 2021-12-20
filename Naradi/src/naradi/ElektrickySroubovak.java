e515017cfe1d08bf78a67ba6f81f1d5062fbdf41
cermak <cermak@cermak.dszo>
2021-12-20 14:42:31 +0100

    naradi

---
+++ Naradi/src/naradi/ElektrickySroubovak.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package naradi;
+
+/**
+ *
+ * @author cermak
+ */
+public class ElektrickySroubovak extends Sroubovak{
+    public int KapacitaBaterie;
+    
+    public ElektrickySroubovak(int KapacitaBaterie){
+    this.KapacitaBaterie = KapacitaBaterie;
+    
+    }
+    
+    
+}
---
+++ Naradi/src/naradi/Kladivo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package naradi;
+
+/**
+ *
+ * @author cermak
+ */
+public class Kladivo extends Nastroj{
+    
+    public boolean Obourucni;
+    @Override
+    public String pracuj(){
+    
+    return "Zatloukám";
+    }
+    
+}
