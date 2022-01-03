561df81c1832ace50f170d64ada985ba053e0c8e
cermak <cermak@cermak.dszo>
2022-01-03 14:30:05 +0100

    Equals metoda

---
+++ EqualsMetoda/src/equalsmetoda/Data.java
@@ -0,0 +1,23 @@
+/*
+ *  Licence Tomas Cermak
+ * 
+ */
+package equalsmetoda;
+
+/**
+ *
+ * @author cermak
+ */
+public class Data {
+
+    public String jmeno;
+    public String prijmeni;
+    public int DatumNarozeni;
+
+    public Data(String jmeno, String prijmeni, int DatumNarozeni) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.DatumNarozeni = DatumNarozeni;
+    }
+
+}
---
+++ EqualsMetoda/src/equalsmetoda/EqualsMetoda.java
@@ -0,0 +1,20 @@
+/*
+ *  Licence Tomas Cermak
+ * 
+ */
+package equalsmetoda;
+
+/**
+ *
+ * @author cermak
+ */
+public class EqualsMetoda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
