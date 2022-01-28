78b18045c35de35ef57123f0ddcade786c48fceb
cermak <tcermak4@gmail.com>
2022-01-28 12:50:01 +0100

    inteliJ

---
+++ TestikInteliJ/src/com/company/Data.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Data {
+    private int cislo;
+    private String jmeno;
+
+    public Data() {
+
+    }
+
+    public int getCislo() {
+        return cislo;
+    }
+
+    public void setCislo(int cislo) {
+        this.cislo = cislo;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+}
