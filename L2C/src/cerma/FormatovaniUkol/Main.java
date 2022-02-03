ec13ad70eadc0efcf56c91a6e4c8da5dd6a1a31f
cermak <tcermak4@gmail.com>
2022-02-03 14:25:08 +0100

    rgds

--- L2C/src/cerma/FormatovaniUkol/Main.java
+++ L2C/src/cerma/FormatovaniUkol/Main.java
@@ -1,14 +1,24 @@
 package cerma.FormatovaniUkol;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-
+metoda1("ahoj tomAs Cermak");
     }
     public static void metoda1(String s){
       String uprText= "";
+      s=  s.toLowerCase();
+      String [] slova = s.split(" ");
 
+      for (String word : slova){
+          uprText += word.substring(0, word.length() - 1).toLowerCase() + Character.toUpperCase(word.charAt(word.length() - 1)) + " ";//posledni pismeno velke, zbytek maly
 
+      }
+System.out.println(uprText);
 
     }
+
+
 }
